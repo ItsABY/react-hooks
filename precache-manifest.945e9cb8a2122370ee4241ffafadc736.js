45dc3800ec2a951c3e280e379030dabe25ea73e8
ajeet <ajeetx@email.com>
2020-03-17 07:23:29 +1100

    Updates

---
+++ precache-manifest.945e9cb8a2122370ee4241ffafadc736.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ec24723470e0ec5ed8b83246192741",
+    "url": "/ajeetx/index.html"
+  },
+  {
+    "revision": "56c04b14d1578a025aa6",
+    "url": "/ajeetx/static/css/main.4f0d7ee8.chunk.css"
+  },
+  {
+    "revision": "f967e47e652479c47490",
+    "url": "/ajeetx/static/js/2.8ed35f5f.chunk.js"
+  },
+  {
+    "revision": "56c04b14d1578a025aa6",
+    "url": "/ajeetx/static/js/main.ed2493d7.chunk.js"
+  },
+  {
+    "revision": "9f61d1db0d459403c1d2",
+    "url": "/ajeetx/static/js/runtime~main.74fcc151.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-hooks/precache-manifest.0831e228ff465b68ab2d94736ec4c719.js"
+  "/ajeetx/precache-manifest.945e9cb8a2122370ee4241ffafadc736.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-hooks/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ajeetx/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
